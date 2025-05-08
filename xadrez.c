282e22df2fe918bf7907c41dfbbcbde2259f85ed
Pedro Ricardo <petterrichard@gmail.com>
2025-05-08 01:42:10 +0000

    adicionei o cavalo

--- xadrez.c
+++ xadrez.c
@@ -6,26 +6,49 @@ int main() {
     int casasTorre = 5;
     int casasBispo = 5; 
     int casasRainha = 8;
+    
 
-    printf("Movimento da torre do xadrez:\n");
+    printf("\nMovimento da torre do xadrez:\n");
     for (i=1; i <= casasTorre; i++)//Repete "i" enquanto "i" for menor ou igual a 5, e incrementa "i" a cada volta
 
     {
-        printf("torre movimentando para a direita %d\n", i); 
+        printf("torre movimentando para a direita %d casa(s)\n", i); 
     }    
 
-    printf("Movimento do bispo no xadrez:\n");//  Movimenta o bispo 5 casas cima direita fazendo assim a diagonal
+    printf("\nMovimento do bispo no xadrez:\n");//  Movimenta o bispo 5 casas cima direita fazendo assim a diagonal
     i = 1;
-    while (i <= casasBispo){ // só entra se for verdadeira a condição
-        printf("Bispo movimentou na diagonal para cima e para a direita até avançar %d\n",i);
+    while (i <= casasBispo){
+        printf("Bispo movimentou na diagonal para cima e para a direita até avançar %dcasa(s)\n",i);
         i++; }
     
-    printf("Movimento da rainha no xadrez:\n");
+    printf("\nMovimento da rainha no xadrez:\n");
     i = 1;
-    do{ // entra uma vez de qualquer jeito, depois pergunta se vai de novo 
-        printf("Rainha movimentando para a esquerda %d\n", i);
+    do{
+        printf("Rainha movimentando para a esquerda %d casa(s)\n", i);
         i++;
     } while(i <= casasRainha);
+
+    printf("\nMovimento do cavalo no xadrez:\n");
+
+    int casaBaixo = 2; 
+    int casaEsquerda = 1;
+    int j; 
+
+    for(int movimento = 1; movimento <= 1; movimento++)
+    {
+        
+        for(i = 1; i<= casaBaixo; i++)
+        {
+            printf("Cavalo avança para a casa de baixo %d\n", i);
+            
+        }
+        j =1;
+    while(j <= casaEsquerda)
+    {
+        printf("Cavalo avança para a casa da esquerda %d\n", j);
+        j++;
+    }
+    }
 
     return 0;
 }
