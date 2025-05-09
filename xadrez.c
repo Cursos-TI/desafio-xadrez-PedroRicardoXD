e5f3a48e6ce6d3c0d65315a4fbfa1135538cccaa
Pedro Ricardo <petterrichard@gmail.com>
2025-05-09 18:14:23 +0000

    adicionadoArecursividade

--- xadrez.c
+++ xadrez.c
@@ -1,54 +1,64 @@
 #include <stdio.h>
 
-int main() {
-
-    int i; 
-    int casasTorre = 5;
-    int casasBispo = 5; 
-    int casasRainha = 8;
-    
-
-    printf("\nMovimento da torre do xadrez:\n");
-    for (i=1; i <= casasTorre; i++)//Repete "i" enquanto "i" for menor ou igual a 5, e incrementa "i" a cada volta
-
+void Movimentotorre(int casaAtual, int casaTotal){             // Inicio do comando recursivo da Torre
+    if (casaAtual > casaTotal)
     {
-        printf("torre movimentando para a direita %d casa(s)\n", i); 
-    }    
-
-    printf("\nMovimento do bispo no xadrez:\n");//  Movimenta o bispo 5 casas cima direita fazendo assim a diagonal
-    i = 1;
-    while (i <= casasBispo){
-        printf("Bispo movimentou na diagonal para cima e para a direita até avançar %dcasa(s)\n",i);
-        i++; }
-    
-    printf("\nMovimento da rainha no xadrez:\n");
-    i = 1;
-    do{
-        printf("Rainha movimentando para a esquerda %d casa(s)\n", i);
-        i++;
-    } while(i <= casasRainha);
-
-    printf("\nMovimento do cavalo no xadrez:\n");
-
-    int casaBaixo = 2; 
-    int casaEsquerda = 1;
-    int j; 
-
-    for(int movimento = 1; movimento <= 1; movimento++)
-    {
-        
-        for(i = 1; i<= casaBaixo; i++)
-        {
-            printf("Cavalo avança para a casa de baixo %d\n", i);
-            
-        }
-        j =1;
-    while(j <= casaEsquerda)
-    {
-        printf("Cavalo avança para a casa da esquerda %d\n", j);
-        j++;
+       return;
     }
+    printf("Torre  movimentando para a direita %d\n", casaAtual);
+    Movimentotorre(casaAtual + 1, casaTotal);
+}             // Fim do comando recursivo da torre 
+
+void MovimentoRainha(int casaAtua, int casaTotal){ // Inicio do camando recursivo da rainha
+    if (casaAtua > casaTotal){
+        return;
     }
 
-    return 0;
+    printf("Rainha movimentando para a esquerda %d\n", casaAtua);
+    MovimentoRainha(casaAtua + 1, casaTotal);
+}             // fim do comando recursivo da rainha
+
+void Movimentobispo(int passos, int totalPassos){  // Inicio do camando recursivo do bispo
+    if(passos > totalPassos){
+        return;
+    }
+
+for(int i = 1; i <= 1; i++){
+    for(int j = 1; j <= 1; j++){
+        printf("Bispo movimentando para cima e para a direita %d\n", passos);
+   
+    }
 }
+  
+    Movimentobispo(passos + 1, totalPassos);
+  //Fim do camando recursivo do bispo
+}
+
+int main(){
+
+int casasCima = 2;
+int casasDireita = 1;
+
+ int casasTorre = 5;
+ printf("Movimento da torre no xadrez:\n");
+ Movimentotorre(1, casasTorre);
+
+int casaRainha = 8;
+printf("\nMovimento da rainha no xadrez:\n");
+MovimentoRainha(1, casaRainha);
+
+int casaBispo = 5;
+
+printf("\nMovimento do bispo no xadrez:\n");
+Movimentobispo(1, casaBispo);
+
+printf("\nMovimento do cavalo no xadrez\n");
+for(int i = 1; i <= casasCima; i++){
+        printf("Cavalo anda %d casas(s) pra cima\n", i);
+   }
+   
+    for(int j = 1; j <= casasDireita; j++){
+         printf("Cavalo anda %d casa(s) para direita\n",j);
+        }
+return 0;
+    }
